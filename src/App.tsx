cc639c1958e20ab03382416b058eb9466888b8e1
CezzMartins <cezzmartins@gmail.com>
2022-06-24 00:11:53 -0300

    feat: add layout leftSide

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import styles from './App.module.css';
+import imageLogo from './assets/images/powered.png';
 
 function App() {
+  const [heightField, setHeightField] = useState<number>(0);
+  const [weigthField, setWeigthField] = useState<number>(0);
+
+  const handleCalculeteImc = () =>{
+    if(heightField && weigthField){
+
+    }else{
+      alert("Digite todos os campos!");
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.main}>
+        <header>
+          <div className={styles.headerContainer}>
+            <img src={imageLogo} alt="logo" width={150} />
+          </div>
+        </header>
+        <div className={styles.container}>
+          <div className={styles.leftSide}>
+            <h1>Calcule Seu IMC</h1>
+            <p>IMC é a sigla para Índice de Massa Corpória, parâmetro  adotado pela Organizção Mundial de Saúde para calcular o peso ideal de cada pessoa.</p>
+
+            <input 
+            type="number" 
+            placeholder="Digite Sua Altura ex: 1.5 (em Metros)" 
+            value={heightField > 0 ? heightField : ''} 
+            onChange={ e => setHeightField(parseFloat(e.target.value))} />
+
+            <input 
+            type="number" 
+            placeholder="Digite Seu Peso ex: 1.5 (em Kg)" 
+            value={weigthField > 0 ? weigthField : ''} 
+            onChange={ e => setWeigthField(parseFloat(e.target.value))} />
+
+            <button onClick={handleCalculeteImc}>Calcular</button>
+          </div>
+          <div className={styles.rightSide}>
+            ..
+          </div>
+        </div>
     </div>
   );
 }
